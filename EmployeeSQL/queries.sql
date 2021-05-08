554bf131ee4dc6f09699ee3189544e25bb94ebaf
E-621 <e_mcstotts@yahoo.com>
2021-05-08 07:42:33 -0600

    Complete base homework assignment

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -21,22 +21,63 @@ Select
 	dept_manager.emp_no,
 	employees.last_name,
 	employees.first_name
-From dept_manager
-Inner Join employees 
-	on employees.emp_no = dept_manager.emp_no
-Inner Join  departments
-	on dept_manager.dept_no = departments.dept_no
-Order by dept_name asc;
-
--- List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+From departments
+Join dept_manager
+	on departments.dept_no = dept_manager.dept_no
+Join employees
+	on dept_manager.emp_no = employees.emp_no
+Order by dept_name Asc;
+	
+-- List the department of each employee with the following information: employee number, last name, first name, 
+-----and department name.
+Select
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+From employees
+Join dept_emp
+	on dept_emp.emp_no = employees.emp_no
+Join departments
+	on departments.dept_no = dept_emp.dept_no;
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
+Select
+	employees.first_name,
+	employees.last_name,
+	employees.sex
+From employees
+Where first_name = 'Hercules' and last_name Like 'B%'; 
 
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
-
--- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+Select
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+From employees
+Join dept_emp
+	on dept_emp.emp_no = employees.emp_no
+Join departments
+	on departments.dept_no = dept_emp.dept_no
+Where departments.dept_name = 'Sales';
+-- List all employees in the Sales and Development departments, including their employee number, last name, 
+-----first name, and department name.
+Select
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name
+From employees
+Join dept_emp
+	on dept_emp.emp_no = employees.emp_no
+Join departments
+	on departments.dept_no = dept_emp.dept_no
+Where departments.dept_name = 'Sales' or departments.dept_name = 'Development';
 
 --  In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+Select
+	employees.last_name, count(employees.last_name)
+From employees
+Group by 1
+Order by 2 desc;
